19658b76c16e46313f6e27390f337ba4577bea85
Rishika-1202 <146413824+Rishika-1202@users.noreply.github.com>
2024-10-21 21:35:11 +0530

    Delete React directory

--- React/server.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-require('dotenv').config()
-const dbconfig=require('./config/dbconfig');
-app.use(express.json());
-const userRoute=require('./routes/userRoute');
-app.use('/api/user',userRoute);
-const port=process.env.PORT ||5000;
-app.listen(port,()=>console.log(`listening on port ${port}`));
